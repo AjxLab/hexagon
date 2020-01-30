fefdbb4563719114af4dcf4ffaf6ee30d0b7486e
AjxLab <abe12@mccc.jp>
2020-01-31 04:03:43 +0900

    home画面を表示（コンソール上で）

--- hexagon.py
+++ hexagon.py
@@ -2,20 +2,29 @@
 
 import shutil
 
+logo = \
+'''
+H    H  EEEEEE  X   X   AAAA    GGGG    OOOO   N   N
+H    H  E        X X   AA  AA  G    G  O    O  NN  N
+HHHHHH  EEEEEE    X    AAAAAA  G       O    O  N N N
+H    H  E        X X   AA  AA  G  GGG  O    O  N  NN
+H    H  EEEEEE  X   X  AA  AA   GGGGG   OOOO   N   N
+'''
+len_logo = len(logo.split('\n')[1])
 width = shutil.get_terminal_size()[0]
-hexagon_ASCII = \
-'''
-H    H  EEEEEE  X   X   AAAA    GGGG    OOOO   NN   NN
-H    H  E        X X   AA  AA  G    G  O    O  NNN  NN
-HHHHHH  EEEEEE    X    AAAAAA  G       O    O  NN N NN
-H    H  E        X X   AA  AA  G  GGG  O    O  NN  NNN
-H    H  EEEEEE  X   X  AA  AA   GGGGG   OOOO   NN   NN
-'''
+logo = [' '*((width-len(line))//2) + line for line in logo.split('\n')]
+logo = '\n'.join(logo)
+
+
 print('''
-welcome to
+  Welcome to
 
-o
+%s
 
 
-''')
-print(width)
+  HEXAGON is an interactive chatbot.
+  This implementation by Tatsuya Abe 2020.
+
+
+
+''' % logo)
