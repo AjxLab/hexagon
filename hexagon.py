aa4283b40fbf8fd28f363c9a50006aa0316f0457
AjxLab <abe12@mccc.jp>
2020-01-31 03:52:24 +0900

    add ascii art

---
+++ hexagon.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import shutil
+
+width = shutil.get_terminal_size()[0]
+hexagon_ASCII = \
+'''
+H    H  EEEEEE  X   X   AAAA    GGGG    OOOO   NN   NN
+H    H  E        X X   AA  AA  G    G  O    O  NNN  NN
+HHHHHH  EEEEEE    X    AAAAAA  G       O    O  NN N NN
+H    H  E        X X   AA  AA  G  GGG  O    O  NN  NNN
+H    H  EEEEEE  X   X  AA  AA   GGGGG   OOOO   NN   NN
+'''
+print('''
+welcome to
+
+o
+
+
+''')
+print(width)
