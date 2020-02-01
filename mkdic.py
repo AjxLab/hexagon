f6b62e6056f901db0668bc529a29b9349563db61
AjxLab <abe12@mccc.jp>
2020-02-02 03:16:50 +0900

    add models

---
+++ mkdic.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from gensim.models import word2vec
+
+data = word2vec.Text8Corpus('data/wiki.txt')
+model = word2vec.Word2Vec(data, size=100)
+model.save('model/wiki.model')
+
+print('Successful!!')
+
