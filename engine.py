e8af0a7b3162e84784d868a96becd66d7adcb67b
AjxLab <abe12@mccc.jp>
2020-02-14 04:59:08 +0900

    形態素解析で単語に分割

--- engine.py
+++ engine.py
@@ -2,4 +2,17 @@
 
 from janome.tokenizer import Tokenizer
 
+# 形態素解析でトークン化
+tokenizer = Tokenizer()
 
+def make_reply(text):
+    ## -----*-----  -----*----- ##
+    if text[-1] != "。": text += "。"
+    words = tokenizer.tokenize(text)
+    for w in words:
+        face = w.surface
+        ps = w.part_of_speech.split(',')[0]
+        print(w.part_of_speech.split(','))
+
+
+print(make_reply('私の名前は阿部健太朗です。'))
